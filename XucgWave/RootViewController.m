78de94e96a0ab4e686a4679037bbed62a8d86fee
xucg <xuchenguang@le.com>
2016-09-23 15:56:31 +0800

    adjust wave property

--- XucgWave/RootViewController.m
+++ XucgWave/RootViewController.m
@@ -24,14 +24,15 @@
     
     _waveView.layer.cornerRadius = 100;
     _waveView.layer.masksToBounds = YES;
-    _waveView.layer.borderColor = [[UIColor greenColor] CGColor];
-    _waveView.layer.borderWidth = 1;
+    _waveView.layer.borderWidth = 3;
+    _waveView.layer.borderColor = [UIColor colorWithRed:233.f/255.f green:233.f/255.f blue:233.f/255.f alpha:1].CGColor;
+    _waveView.backgroundColor = [UIColor colorWithRed:33.f/255.f green:33.f/255.f blue:33.f/255.f alpha:1];
     
     _wave1 = [[XucgWave alloc] initWidth:200 height:200];
     _wave1.fillColor = [UIColor cyanColor].CGColor;
     _wave1.percent = 0.5f;
     _wave1.growSpeed = 0.01;
-    _wave1.waveLength = 100;
+    _wave1.waveLength = 150;
     [_waveView.layer addSublayer:_wave1];
     
     _wave2 = [[XucgWave alloc] initWidth:200 height:200];
@@ -40,6 +41,7 @@
     _wave2.growSpeed = 0.01;
     _wave2.waveLength = 100;
     _wave2.startX = 50;
+    _wave2.waveSpeed = 5;
     [_waveView.layer addSublayer:_wave2];
 }
 
